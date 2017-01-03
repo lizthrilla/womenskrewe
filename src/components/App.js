dd064ce0799d5f7323f38b9019946cbb8f5bfcda
Liz Tiller <elizabeth.tiller@gmail.com>
2017-01-03 16:40:28 -0500

    work damnit

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 class App extends Component {
 
   render () {
-    return <div>
-      <h1>Tampa Bay Women's Krewe RFC </h1>
+    return <div className='root'>
+      <div className='header'>
+        <h1>Tampa Bay Women's Krewe RFC </h1>
+      </div>
       <nav>
         <ul>
           <li>Home</li>
@@ -14,7 +16,11 @@ class App extends Component {
           <li>Sponsors</li>
         </ul>
       </nav>
-      <div className='recruit' />
+      <div className='recruit'>
+        <h2>Now Recruiting!</h2>
+        <p> Practices are Tuesday and Thursday 7:30 - 9:30 PM at Cuscaden Park in Ybor. </p>
+        <div className='practice' />
+      </div>
       <div className='contact' />
       <div className='calendar' />
       <div className='scores' />
