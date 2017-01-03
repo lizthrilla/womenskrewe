7411614bee9d193af1183fa7bad57ffc3ab58830
Liz Tiller <elizabeth.tiller@gmail.com>
2017-01-03 16:07:08 -0500

    css fixed

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,24 @@ class App extends Component {
 
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <h1>Tampa Bay Women's Krewe RFC </h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Calendar</li>
+          <li>Scores</li>
+          <li>Sponsors</li>
+        </ul>
+      </nav>
+      <div className='recruit' />
+      <div className='contact' />
+      <div className='calendar' />
+      <div className='scores' />
+      <div className='sponsorship' />
+      <footer>
+        <h6>&copy; 2017 Liz Tiller for Tampa Bay Women's Krewe RFC</h6>
+      </footer>
     </div>
   }
 }
