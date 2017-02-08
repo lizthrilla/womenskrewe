a75f0f96f420577cbbc97102e3b606c08db92857
Liz Tiller <elizabeth.tiller@gmail.com>
2017-02-08 18:49:05 -0500

    fixed schedule page

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,10 @@ class Home extends Component {
         <h1>Tampa Bay Krewe Women's Rugby</h1>
       </div>
       <div className='teamPhoto'>
-        <img src='./src/kreweTeam.JPG' />
+        <img src='./src/kreweTeam2.jpg' />
       </div>
       <div className='info'>
-        <p> The Tampa Bay Krewe Women's RFC is a club comprised of women living in the Greater Tampa Bay Area. We are women of every shape, size, color and age. We come from all over the United States. Diverse in almost every possible way, there is one thing that bonds us together: our love and dedication to the sport of rugby. </p>
-
-        <p>Our goal is to continue the outreach and the growth of women's rugby in Florida, all while bringing together and empowering women. We welcome anyone and everyone, regardless of experience or skill level. Help us reach our goals, come join us!</p>
+        <p> The Tampa Bay Krewe Women's RFC is a club comprised of women living in the Greater Tampa Bay Area. We are women of every shape, size, color and age. We come from all over the United States. Diverse in almost every possible way, there is one thing that bonds us together: our love and dedication to the sport of rugby. Our goal is to continue the outreach and the growth of women's rugby in Florida, all while bringing together and empowering women. We welcome anyone and everyone, regardless of experience or skill level. Help us reach our goals, come join us!</p>
       </div>
       <div className='buttons'>
         <button className='facebook'>
